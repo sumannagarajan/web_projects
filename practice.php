89ea1da18466aae29b35997dfa582ef55fb540f0
sumannagarajan <sumannagarajan@proton.me>
2024-01-25 11:33:17 +0530

    constant case sencetive

---
+++ practice.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>sample.page.com</title>
+    </head>
+    <body>
+        <h2>heloow world PHP is fun</h2>
+        <?php
+//contant case sencetive
+
+define("a", 10);
+echo a;
+
+?>
+    </body>
+</html>
