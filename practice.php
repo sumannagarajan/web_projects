d97d8851d97d5df16326dffefd5906e44c6bee10
sumannagarajan <sumannagarajan@proton.me>
2024-01-25 20:23:08 +0530

    ifelse statement

--- practice.php
+++ practice.php
@@ -6,29 +6,11 @@
     <body>
         <h2>heloow world PHP is fun</h2>
         <?php
-//constant case sencetive
-
-define("a", 10);
-echo a;
-echo "<br>";
-
-//constant key word
-const b = 90;
-echo b;
-echo "<br>";
-
-// contants in array
-
-define("myfav_fruits", ["mango", "banana", "apple", "orange"]);
-echo myfav_fruits[0];
-echo "<br>";
-echo myfav_fruits[2];
-echo "<br>";
-
-//if statement in php
-
-if (1) {
-    echo "good morning.....";
+$a = 10;
+if ($a > 5) {
+    echo "YES A is above 10";
+} else {
+    echo "some thing went wrong";
 }
 
 ?>
