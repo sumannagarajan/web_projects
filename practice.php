fb0bc814bcea04efd0ae9025c2d62ffc3bc7209a
sumannagarajan <sumannagarajan@proton.me>
2024-01-26 19:45:04 +0530

    nestedif statement

--- practice.php
+++ practice.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>sample.page.com</title>
-    </head>
-    <body>
-        <h2>heloow world PHP is fun</h2>
-        <?php
-// if elseif
+<body>
 
+
+<?php
+
+// nested if statement
 $a = 20;
-if ($a > 20) {
-    echo "a is greater than 20";
-} elseif ($a == 20) {
+if ($a == 20) {
     echo "yes a is equal to 20";
+    if ($a > 20) {
+        echo "yes a is greater than 20";
+    } else {
+        echo "something went wrong";
+    }
 }
 ?>
-    </body>
+
+</body>
 </html>
