83248043d1746e4980ee4aa9485600399a2e7591
sumannagarajan <sumannagarajan@proton.me>
2024-01-27 11:06:40 +0530

    user defined function

--- practice.php
+++ practice.php
@@ -5,19 +5,14 @@
 
 <?php
 
-/*
-for loop ---->if we know the range of value
-(or) if we know how many times the code will
-execute means then we go for "forloop" for is
-best loop if we know the range.
- */
+// user defined function
 
-// using forloop to display 3rd tables
-$b = 3;
-for ($a = 1; $a <= 10; $a++) {
-    $c = $a * $b;
-    echo " $a X $b = $c <br>";
+function mymsg()
+{
+    echo "hello user have a good day";
 }
+
+mymsg();
 ?>
 </body>
 </html>
