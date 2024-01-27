155fd61b04ff8108bd01bcfecd41060697fdfb70
sumannagarajan <sumannagarajan@proton.me>
2024-01-27 10:14:08 +0530

    for loop for numbers displaying

--- practice.php
+++ practice.php
@@ -5,13 +5,16 @@
 
 <?php
 
-//do while loop---------->it will execute a block of code at least once and check the condition is true
-$a = 0;
-do {
-    echo $a;
-    $a++;
-} while ($a < 10);
+/*
+for loop ---->if we know the range of value
+(or) if we know how many times the code will
+execute means then we go for "forloop" for is
+best loop if we know the range.
+ */
 
+for ($a = 0; $a <= 10; $a++) {
+    echo "The number is : $a <br>";
+}
 ?>
 </body>
 </html>
