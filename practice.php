cc876889b7d3efcca2fd9d70dec7df6869eb1636
sumannagarajan <sumannagarajan@proton.me>
2024-01-27 09:16:31 +0530

    while loop

--- practice.php
+++ practice.php
@@ -4,27 +4,13 @@
 
 
 <?php
-
-// switch statement
-
-$favcolor = "blue";
-switch ($favcolor) {
-    case "red";
-        echo "your fav color is red";
-        break;
-    case "blue";
-        echo "your fav color is blue";
-        break;
-    case "orange";
-        echo "your fav color is orange";
-        break;
-    default:
-        echo "something went wrong";
-
+//while loop
+$a = 1;
+while ($a < 10) {
+    echo $a;
+    $a++;
 }
 
 ?>
-
-<h2>sample heading 2</h2>
 </body>
 </html>
