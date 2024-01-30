74a529782122cfd76b554324ea68cdc034871671
sumannagarajan <sumannagarajan@proton.me>
2024-01-30 20:31:35 +0530

    practicing variadic function

--- practice.php
+++ practice.php
@@ -1,14 +1,29 @@
 <?php
-//example for variadic function
+
+/*//example for variadic function
 function sumarr(...$x)
 {
-    $len = count($x);
-    $n = 0;
-    for ($i = 0; $i < $len; $i++) {
-        $n += $x[$i];
-    }
-    return $n; //It will retunrs sum of array
+$len = count($x);
+$n = 0;
+for ($i = 0; $i < $len; $i++) {
+$n += $x[$i];
+}
+return $n; //It will retunrs sum of array
 
 }
 $a = sumarr(10, 20, 30, 40, 50);
-echo "$a";
+echo "$a"; */
+
+//practicing with variadic function
+
+function myfamily($lastname, ...$firstname)
+{
+    $txt = "";
+    $l = count($firstname);
+    for ($i = 0; $i < $l; $i++) {
+        $txt = $txt . "hi $firstname[$i] $lastname <br>";
+    }
+    return $txt;
+}
+$v = myfamily("nagarajan", "kanchana", "suman", "varsha");
+echo $v;
