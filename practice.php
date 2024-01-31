c82a7a7add75a3aa1aa0a1aeee8802ebec078ae8
sumannagarajan <sumannagarajan@proton.me>
2024-01-31 20:15:55 +0530

    strict declaration

--- practice.php
+++ practice.php
@@ -1,29 +1,8 @@
 <?php
-
-/*//example for variadic function
-function sumarr(...$x)
+//strict declaration
+declare (strict_types = 1);
+function sumadd(int $a, float $b): float
 {
-$len = count($x);
-$n = 0;
-for ($i = 0; $i < $len; $i++) {
-$n += $x[$i];
+    return $a + $b;
 }
-return $n; //It will retunrs sum of array
-
-}
-$a = sumarr(10, 20, 30, 40, 50);
-echo "$a"; */
-
-//practicing with variadic function
-
-function myfamily($lastname, ...$firstname)
-{
-    $txt = "";
-    $l = count($firstname);
-    for ($i = 0; $i < $l; $i++) {
-        $txt = $txt . "hi $firstname[$i] $lastname <br>";
-    }
-    return $txt;
-}
-$v = myfamily("nagarajan", "kanchana", "suman", "varsha");
-echo $v;
+echo sumadd(12, 109.4);
