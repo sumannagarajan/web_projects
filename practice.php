3c791453865aa4d94295b15bb2fd948f80098663
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 21:27:34 +0530

    2D array and accessing array element by its index

--- practice.php
+++ practice.php
@@ -1,5 +1,26 @@
-<?php
-//array sorting by using sort function
-$age = array("suman" => "23", "aravind" => "12", "krishna" => "10");
-arsort($age); //sorts an associative array in decending order, according to the value
-print_r($age);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>practice.me</title>
+    </head>
+    <body>
+        <h2>THIS IS SAMPLE PAGE</h1>
+        <pre>
+        <?php
+//multidimensional array
+//2D array.
+$student_details = array(
+    array("suman", "age22", "20ecl236"),
+    array("surya", "age23", "20ecl237"),
+    array("viknesh", "age 24", "20ecl241"),
+);
+//display the arrays content.
+print_r($student_details);
+//accessing the 2D array.
+echo $student_details[0][2] . "<br>";
+echo $student_details[2][2] . "<br>"
+
+?>
+        </pre>
+  </body>
+</html>
