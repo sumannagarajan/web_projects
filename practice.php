675f829f44291457445bb909834944c5e53577e2
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 13:21:11 +0530

    remove array by using splice function

--- practice.php
+++ practice.php
@@ -1,5 +1,6 @@
 <?php
-// remove array multiple items
+// remove array by using splice function
+//it will re arrage the array in unset function will not re-arrange the array
 $randomnames = array("suman", "viknesh", "arun", "vimal", "nantha", "surya", "arul", "sri");
-unset($randomnames[5], $randomnames[0], $randomnames[1]);
+array_splice($randomnames, 0, 1); //it will delete the 0th iteam
 print_r($randomnames);
