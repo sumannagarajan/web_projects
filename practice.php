7d6bc1dd868d2fc214957dd288ca97e79dc930c4
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 15:20:05 +0530

    associative array in decending order, according to the value

--- practice.php
+++ practice.php
@@ -1,5 +1,5 @@
 <?php
 //array sorting by using sort function
 $age = array("suman" => "23", "aravind" => "12", "krishna" => "10");
-krsort($age); //sorts an associative array in decending order, according to the key:
+arsort($age); //sorts an associative array in decending order, according to the value
 print_r($age);
