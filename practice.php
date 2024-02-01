7e70c2037c43cc1103d73290c04228c632072ae6
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 15:07:33 +0530

    sort decending

--- practice.php
+++ practice.php
@@ -1,5 +1,5 @@
 <?php
 //array sorting by using sort function
 $favnum = array(7, 5, 3, 10, 6, 2);
-sort($favnum); // here it will sort the numbers in ascending order
+rsort($favnum); // here it will sort the numbers in decending order
 print_r($favnum);
