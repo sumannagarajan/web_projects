c2896bbaeec65aff72fe6dfeb80bf68c720c2fdb
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 10:33:17 +0530

    loop through an array

--- practice.php
+++ practice.php
@@ -1,6 +1,6 @@
 <?php
-// change value in indexed array
+// loop through an array
 $stuname = array("suman", "ram", "gopal", "shiva", "mohan", "arun", "bala", "hariharan", "viknesh", "subash");
-$stuname[0] = "sumannagarajan";
-$stuname[1] = "ramkumar";
-print_r($stuname);
+foreach ($stuname as $a) {
+    echo $a . "<br>\n";
+}
