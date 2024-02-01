69d55de291d64cada51db66df6dd00e560d8f3a4
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 13:24:59 +0530

    remove item from associative array

--- practice.php
+++ practice.php
@@ -1,6 +1,5 @@
 <?php
-// remove array by using splice function
-//it will re arrage the array in unset function will not re-arrange the array
-$randomnames = array("suman", "viknesh", "arun", "vimal", "nantha", "surya", "arul", "sri");
-array_splice($randomnames, 0, 1); //it will delete the 0th iteam
-print_r($randomnames);
+//Remove itemfrom associatice array
+$cars = array("name" => "honda", "model" => "city", "year" => "1995");
+unset($cars["year"]);
+print_r($cars);
