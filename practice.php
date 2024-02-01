4fd303f595ba2aa4cd353a4bcec3711501713952
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 12:57:14 +0530

    update array items in foreach loop with unset function

--- practice.php
+++ practice.php
@@ -1,7 +1,10 @@
 <?php
-//Associative array
-//Loop through an array by using foreach loop
-$studetails = array("name" => "suman", "rollno" => "20ecl236", "dob" => "07-09-2001", "address" => "erode district");
-foreach ($studetails as $a => $b) {
-    echo "$a : $b <br>\n";
+//update array items in foreach loop
+$favsanks = array("chips", "biscut", "carmelcandies", "piza", "burger", "bondas");
+foreach ($favsanks as &$x) {
+    $x = "potato chips";
+
 }
+unset($x); //with unset function
+//$x = "cookkies";
+print_r($favsanks);
