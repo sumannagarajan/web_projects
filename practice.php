8b3eada301b3a02379245952482ce63554e2893c
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 10:19:12 +0530

    accessing indexed array

--- practice.php
+++ practice.php
@@ -1,8 +1,6 @@
 <?php
-// practicing simple array
-
+// Accessing  indexed array
 $stuname = array("suman", "ram", "gopal", "shiva", "mohan", "arun", "bala", "hariharan", "viknesh", "subash");
-$a = 10;
-$b = "hello";
-var_dump($stuname); //This function is used to display the details about value like which data types
-print_r($stuname); // This function is used to display the content inside the array
+echo $stuname[0] . "<br>";
+echo $stuname[1] . "<br>";
+echo $stuname[2] . "<br>";
