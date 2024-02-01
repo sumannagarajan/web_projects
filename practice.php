1c2f716429c16679a6d954a8fd74d8b93f14f2c9
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 10:28:22 +0530

    change value in array

--- practice.php
+++ practice.php
@@ -1,6 +1,6 @@
 <?php
-// Accessing  indexed array
+// change value in indexed array
 $stuname = array("suman", "ram", "gopal", "shiva", "mohan", "arun", "bala", "hariharan", "viknesh", "subash");
-echo $stuname[0] . "<br>";
-echo $stuname[1] . "<br>";
-echo $stuname[2] . "<br>";
+$stuname[0] = "sumannagarajan";
+$stuname[1] = "ramkumar";
+print_r($stuname);
