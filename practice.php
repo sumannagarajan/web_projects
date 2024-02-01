5af20f347286ff21f779eb5ea2d0d90f11ab1651
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 13:04:27 +0530

    remove array multile items

--- practice.php
+++ practice.php
@@ -1,5 +1,5 @@
 <?php
-// remove array item
+// remove array multiple items
 $randomnames = array("suman", "viknesh", "arun", "vimal", "nantha", "surya", "arul", "sri");
-unset($randomnames[5]);
+unset($randomnames[5], $randomnames[0], $randomnames[1]);
 print_r($randomnames);
