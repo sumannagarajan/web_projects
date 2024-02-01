a9f0f01a071e449d78176bf64176b1544430a475
sumannagarajan <sumannagarajan@proton.me>
2024-02-01 15:17:33 +0530

    associative array in ascending order, according to the key

--- practice.php
+++ practice.php
@@ -1,5 +1,5 @@
 <?php
 //array sorting by using sort function
 $age = array("suman" => "23", "aravind" => "12", "krishna" => "10");
-asort($age);
+ksort($age); //sorts an associative array in ascending order, according to the key:
 print_r($age);
