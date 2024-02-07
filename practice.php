9bcb77077e308eb8fc9b2916fa64c965adb7f38e
sumannagarajan <sumannagarajan@proton.me>
2024-02-07 22:49:02 +0530

    Global variables

--- practice.php
+++ practice.php
@@ -7,20 +7,14 @@
         <h2>THIS IS SAMPLE PAGE</h1>
         <pre>
         <?php
-//multidimensional array
-//2D array.
-$student_details = array(
-    array("suman", "age22", "20ecl236"),
-    array("surya", "age23", "20ecl237"),
-    array("viknesh", "age 24", "20ecl241"),
-);
-//loop through the 2D array
-for ($i = 0; $i < 3; $i++) {
-    echo "row value is $i<br>";
-    for ($j = 0; $j < 3; $j++) {
-        echo $student_details[$i][$j] . "<br>";
-    }
-}
+print("_SERVER \n");
+print_r($_SERVER);
+print("_GET \n");
+print_r($_GET);
+print("_POST \n");
+print_r($_POST);
+print("_FILES");
+print_r($_FILES);
 ?>
     </pre>
   </body>
